1b2664dc7a412cb165e3e67b055c2c227f275dba
zoe <zoe.koksi.saunders@gmail.com>
2025-07-13 14:17:02 -0400

    player scene + script merge
    - committing .tscn and .gd player files to main

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,6 @@ extends CharacterBody2D
 
 const SPEED = 100.0
 
-
 func _physics_process(_delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if direction.length():
