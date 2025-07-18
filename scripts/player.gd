8e499c3e3cdab26724373968d2325d1f7ea5424e
zoe <zoe.koksi.saunders@gmail.com>
2025-07-17 21:51:49 -0400

    walking sprite + movement framework updated
    generally edited scripts to connect animation to movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,30 @@
 extends CharacterBody2D
 
-const SPEED = 100.0
+const SPEED = 80.0
+@onready var sprite: AnimatedSprite2D = $Movement
 
 func _physics_process(_delta):
+	#get input direction
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	if direction.length():
 		velocity = direction * SPEED
+		play_animation(direction)
 		#look_at(position + direction)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+		sprite.play("Idle")
+		
 	move_and_slide()
+
+func play_animation(dir: Vector2):
+	if abs(dir.x) > abs(dir.y):
+		if dir.x > 0:
+			sprite.play("Idle")
+		else:
+			sprite.play("Idle")
+	else:
+		if dir.y > 0:
+			sprite.play("WalkDown")
+		else:
+			sprite.play("Idle")
